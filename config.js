50e334891e3c698fd7694b93fe2b47d3b11ee6fd
TZOUVELI PARASKEVI <39123087+parasktz@users.noreply.github.com>
2026-06-06 11:41:53 +0300

    Update Firebase configuration in config.js
    Updated Firebase configuration details for the project.

--- config.js
+++ config.js
@@ -3,11 +3,11 @@
 // ║  Project Settings →      Your apps → Web app → Config     ║
 // ╚══════════════════════════════════════════════════════╝
 const FIREBASE_CONFIG = {
-  apiKey: "AIzaSyA_aHBdW2p5BwfZZhwYK99ybWr2aOPS8fs",
-  authDomain: "cultureboteval.firebaseapp.com",
-  databaseURL: "https://cultureboteval-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "cultureboteval",
-  storageBucket: "cultureboteval.firebasestorage.app",
-  messagingSenderId: "463119543508",
-  appId: "1:463119543508:web:581f7336399af2d7de1f41"
+  apiKey: "AIzaSyBdAaZ_GgK4aRzq5Gp4T9nlOMFmcyaG3aw",
+  authDomain: "cultureboteval-v2-18d9e.firebaseapp.com",
+  databaseURL: "https://cultureboteval-v2-18d9e-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "cultureboteval-v2-18d9e",
+  storageBucket: "cultureboteval-v2-18d9e.firebasestorage.app",
+  messagingSenderId: "283112596485",
+  appId: "1:283112596485:web:99550da05b22c7ab7a286c"
 };
